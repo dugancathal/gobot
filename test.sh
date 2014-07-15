c1d7d36af72187645fdd5c9e42c3ae26dd5239d4
Arjun Sharma <rgzsharma@gmail.com>
2014-07-14 23:43:15 -0600

    Added a test runner bash script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd acceptance
+echo "##############################"
+echo "   Running Acceptance Specs   "
+echo "##############################"
+bundle exec rspec spec --format documentation
+echo "##############################"
+echo "   Done Running Specs   "
+echo "##############################"
+cd - > /dev/null
